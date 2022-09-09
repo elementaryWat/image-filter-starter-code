383e8f859098d5d4c9b30d5dd266ec44f9b3c470
Augusto Romero <augustoromerodev@gmail.com>
2022-09-09 13:35:00 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get( "/filteredimage", async ( req, res ) => {
+    try {
+      let { image_url } = req.query;
+      let filteredpath = await filterImageFromURL(image_url);
+      res.status(200).sendFile(filteredpath,()=>{deleteLocalFiles([filteredpath])});
+
+    }
+    catch (error) {
+      res.status(400).send(error);
+    }
+  } );
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
