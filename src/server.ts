061b12dbebbadee0431acca261886121215931d8
Augusto Romero <augustoromerodev@gmail.com>
2022-09-09 18:08:56 -0300

    TS added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import {Application, Request, Response, NextFunction, Errback} from "express";
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { Console } from 'console';
 
 (async () => {
 
@@ -13,18 +15,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async (req: Request, res: Response) => {
     try {
-      let { image_url } = req.query;
-      let filteredpath = await filterImageFromURL(image_url);
+      let filteredpath: string = await filterImageFromURL(req.query.image_url as string);
       res.status(200).sendFile(filteredpath,()=>{deleteLocalFiles([filteredpath])});
-
     }
     catch (error) {
+      console.log(error);
       res.status(400).send("Invalid URL");
     }
   } );
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
